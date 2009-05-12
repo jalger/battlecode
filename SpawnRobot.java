0acea8fc1a46bee17981707f478d505e1384870a
Nicholas C Dunn <ndunn2@bowdoin.edu>
2009-05-12 11:22:11 -0400

    Archon now heals the robot it just spawned.  Sort of.

--- SpawnRobot.java
+++ SpawnRobot.java
@@ -6,18 +6,52 @@ import battlecode.common.*;
 
 public class SpawnRobot extends DefaultRobot {
     
+    private boolean justSpawned;
+    
+    
     public SpawnRobot(RobotController rcIn) {
         super(rcIn);
+        justSpawned = false;
     }
 
     public void run() {
         System.out.println("I'm an Archon!");
         try {
             while (true) {
+                
+                if (justSpawned) {
+                    MapLocation oneSquareForward = rc.getLocation().add(rc.getDirection());
+                    Robot spawned = rc.senseGroundRobotAtLocation(oneSquareForward);
+                    if (spawned != null) {
+                        RobotInfo spawnedInfo = rc.senseRobotInfo(spawned);
+                        
+                        // It's not fully healed
+                        if (spawnedInfo.eventualEnergon < spawnedInfo.maxEnergon) {
+                            double amountNeeded = spawnedInfo.maxEnergon - spawnedInfo.energonLevel;
+                            // Don't kill yourself to heal the guy
+                            double maxToTransfer = Math.min(rc.getEnergonLevel(), amountNeeded);
+                            
+                            rc.transferEnergon(maxToTransfer,
+                                                oneSquareForward,
+                                                RobotLevel.ON_GROUND);
+                        }
+                    }
+                }
+                
+                // Check to see if any robots around you need energon
+                
+                
+                    // If they do, and they're adjacent, heal them
+                
+                    // Else, move in their direction
+                
+                // Else if you can spawn a unit, do so
+                
                 // Check to see if square in front of you is free
-                if (canSpawn(rc, RobotType.SOLDIER)) {
+                else if (canSpawn(rc, RobotType.SOLDIER)) {
                     try {
                         rc.spawn(RobotType.SOLDIER);
+                        justSpawned = true;
                     }
                     catch (Exception e) {
                         e.printStackTrace();
@@ -42,8 +76,14 @@ public class SpawnRobot extends DefaultRobot {
     }
 
     /**
+    * Checks whether creating the requested robot will succeed.
+    *
+    * Assumes that you're not being stupid, e.g. you are not calling
+    * this with a non-archon, you are not trying to spawn an evolved
+    * unit type, and you haven't already queued an action this round.
     * @return true if spawning a robot of <code>RobotType</code> will
-    * succeed
+    * succeed (subject to limitations outlined above)
+    *
     **/
     public static boolean canSpawn(RobotController rc, RobotType t) {
         // Not in range of a tower
