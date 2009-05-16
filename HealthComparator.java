40467c0b589eb955779b96b680495dd562ff5516
Nicholas C Dunn <ndunn2@bowdoin.edu>
2009-05-15 20:27:13 -0400

    Added two comparators for sorting robotInfos - distance and health.

---
+++ HealthComparator.java
@@ -0,0 +1,28 @@
+package teamJA_ND;
+
+import java.util.Comparator;
+import battlecode.common.MapLocation;
+import battlecode.common.RobotInfo;
+import battlecode.common.RobotController;
+
+
+
+public class HealthComparator implements Comparator<RobotInfo> {
+    
+    /**
+    * Compare the amount of health each robot has
+    **/
+    public int compare(RobotInfo r1, RobotInfo r2) {
+        double proportionOfHealth1 = SpawnRobot.getProportionHealth(r1);
+        double proportionOfHealth2 = SpawnRobot.getProportionHealth(r2);
+        if (proportionOfHealth1 == proportionOfHealth2) {
+            return 0;
+        }
+        else if (proportionOfHealth1 < proportionOfHealth2) {
+            return -1;
+        }
+        else {
+            return 1;
+        }
+    }
+}
--- SpawnRobot.java
+++ SpawnRobot.java
@@ -3,6 +3,7 @@ import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
 import battlecode.common.GameActionException;
 import battlecode.common.*;
+import teamJA_ND.util.UtilityFunctions;
 
 import java.util.List;
 import java.util.LinkedList;
@@ -13,7 +14,11 @@ public class SpawnRobot extends DefaultRobot {
     
     private int numRobotsSpawned;
     private boolean justSpawned;
-    private static final double MIN_ENERGON_RESERVE = 10D;
+    private static final double MIN_ENERGON_RESERVE = 10.0;
+    // Don't transfer energon if you have less than 40% life
+    private static final double MIN_PROPORTION_ENERGON_TO_HEAL = 0.4;
+    
+    private static final double MAX_ENERGON = RobotType.ARCHON.maxEnergon();
     
     private static final int MAX_NUM_SOLDIERS_TO_SPAWN = 2;
     
@@ -29,8 +34,11 @@ public class SpawnRobot extends DefaultRobot {
         Direction.NONE
     };
     
+    public static final boolean DEBUG = true;
     
     private List<Robot> children;
+    
+    private DistanceComparator distanceComp;
     
     
     public SpawnRobot(RobotController rcIn) {
@@ -38,8 +46,9 @@ public class SpawnRobot extends DefaultRobot {
         healingTarget = null;
         numRobotsSpawned = 0;
         justSpawned = false;
-        
         children = new LinkedList<Robot>();
+        
+        distanceComp = new DistanceComparator(rc);
     }
 
     public void run() {
@@ -63,6 +72,8 @@ public class SpawnRobot extends DefaultRobot {
         try {
             while (true) {
                 
+                startTurn();
+                
                 
                 if (children.size() < MAX_NUM_SOLDIERS_TO_SPAWN &&
                     canSpawn(rc, RobotType.SOLDIER)) {
@@ -71,6 +82,9 @@ public class SpawnRobot extends DefaultRobot {
                     justSpawned = true;
                     rc.yield();
                 }
+                
+                
+                
                 
                 // Unit should appear in front of me
                 if (justSpawned) {
@@ -86,69 +100,73 @@ public class SpawnRobot extends DefaultRobot {
                     }
                 }
                 
+                
+                
                 // Don't heal anyone until you've spawned at least one unit
                 if (children.size() > 0) {
-                    
-                    // Try to find robots to heal
-                    if (healingTarget == null) {
-                        //debug_tick();
-                        // Check to see if any robots around you need energon
-                        Robot[] groundUnits = rc.senseNearbyGroundRobots();
-                    
-                        healingTarget = getAdjacentFriendlyRobot(rc, groundUnits);
-                        //healingTarget = getClosestDamagedFriendlyRobot(rc, groundUnits);
-                        //healingTarget = getMostDamagedRobot(rc, groundUnits);
-                    }
                 
-                    // We have a target we're trying to heal
-                    if (healingTarget != null) {
-                    
-                    
-                        System.out.println("Attempting to heal " + healingTarget);
-                    
-                        // TODO: We need to ensure that our target does not move
-                        // out of range from the time we choose it, and then
-                        // try to sense it.  Otherwise we'll get an exception
-                    
-                    
-                        // Find out information about the robot we're going to
-                        // heal
-                        RobotInfo healingTargetInfo = rc.senseRobotInfo(healingTarget);
-                    
-                        MapLocation curLoc = rc.getLocation();
-                        MapLocation healingTargetLoc = healingTargetInfo.location;
-                    
-                        // We can transfer the energon
-                        if (curLoc.equals(healingTargetLoc) || 
-                            curLoc.isAdjacentTo(healingTargetLoc)) {
-                        
-                            double energonAmt = Math.min(rc.getEnergonLevel() - 
-                                                        MIN_ENERGON_RESERVE, 
-                                                        getDamage(healingTargetInfo));
-                                                    
-                            if (energonAmt < 0) {
-                                energonAmt = 0;
-                            }                            
-                            rc.transferEnergon( energonAmt, 
-                                                healingTargetLoc,
-                                                healingTarget.getRobotLevel()); 
-                        
-                            // Ensure we don't try to do two actions this turn
-                            healingTarget = null;
-                            rc.yield();
-                        }
-                    
-                        // We need to move towards the robot
-                        else {
-                            System.out.println("I am at " + curLoc + " and need to move to reach " + healingTargetLoc);
-                        
-                            //moveTo(healingTargetLoc);
-                            continue;
-                        }
-                    
-                    
-                    }
+                    heal();
                 }
+                    // // Try to find robots to heal
+                    //                     if (healingTarget == null) {
+                    //                         //debug_tick();
+                    //                         // Check to see if any robots around you need energon
+                    //                         Robot[] groundUnits = rc.senseNearbyGroundRobots();
+                    //                     
+                    //                         healingTarget = getAdjacentFriendlyRobot(rc, groundUnits);
+                    //                         //healingTarget = getClosestDamagedFriendlyRobot(rc, groundUnits);
+                    //                         //healingTarget = getMostDamagedRobot(rc, groundUnits);
+                    //                     }
+                    //                 
+                    //                     // We have a target we're trying to heal
+                    //                     if (healingTarget != null) {
+                    //                     
+                    //                     
+                    //                         System.out.println("Attempting to heal " + healingTarget);
+                    //                     
+                    //                         // TODO: We need to ensure that our target does not move
+                    //                         // out of range from the time we choose it, and then
+                    //                         // try to sense it.  Otherwise we'll get an exception
+                    //                     
+                    //                     
+                    //                         // Find out information about the robot we're going to
+                    //                         // heal
+                    //                         RobotInfo healingTargetInfo = rc.senseRobotInfo(healingTarget);
+                    //                     
+                    //                         MapLocation curLoc = rc.getLocation();
+                    //                         MapLocation healingTargetLoc = healingTargetInfo.location;
+                    //                     
+                    //                         // We can transfer the energon
+                    //                         if (curLoc.equals(healingTargetLoc) || 
+                    //                             curLoc.isAdjacentTo(healingTargetLoc)) {
+                    //                         
+                    //                             double energonAmt = Math.min(rc.getEnergonLevel() - 
+                    //                                                         MIN_ENERGON_RESERVE, 
+                    //                                                         getDamage(healingTargetInfo));
+                    //                                                     
+                    //                             if (energonAmt < 0) {
+                    //                                 energonAmt = 0;
+                    //                             }                            
+                    //                             rc.transferEnergon( energonAmt, 
+                    //                                                 healingTargetLoc,
+                    //                                                 healingTarget.getRobotLevel()); 
+                    //                         
+                    //                             // Ensure we don't try to do two actions this turn
+                    //                             healingTarget = null;
+                    //                             rc.yield();
+                    //                         }
+                    //                     
+                    //                         // We need to move towards the robot
+                    //                         else {
+                    //                             System.out.println("I am at " + curLoc + " and need to move to reach " + healingTargetLoc);
+                    //                         
+                    //                             //moveTo(healingTargetLoc);
+                    //                             continue;
+                    //                         }
+                    //                     
+                    //                     
+                    //                     }
+                    //                 }
                 
                 
                 if (!rc.isMovementActive()) {
@@ -295,5 +313,119 @@ public class SpawnRobot extends DefaultRobot {
     public static double getDamage(RobotInfo r) {
         return r.maxEnergon - r.eventualEnergon;
     }
+    
+    /**
+    * @return the proportion of life the robot has left
+    **/
+    public static double getProportionHealth(RobotInfo r) {
+        return r.eventualEnergon / r.maxEnergon;
+    }
+
+    /**
+    * @return the proportion of life the robot has left
+    **/
+    public static double getProportionHealth(RobotController rc) {
+        return rc.getEventualEnergonLevel() / rc.getMaxEnergonLevel();
+    }
+
+
+    /**
+    * @return true if we have enough energon to attempt to heal other units
+    **/
+    private boolean shouldHeal() {
+        double proportionHealth = rc.getEventualEnergonLevel() / MAX_ENERGON;
+        return proportionHealth >= MIN_PROPORTION_ENERGON_TO_HEAL;
+    }
+
+    /**
+    *
+    * Transfer some of your energon to a nearby robot that has lower health, 
+    * as a percentage.
+    * Note that this method assumes that the knowledge base is up to date;
+    * if the robots around it have moved and we attempt to transfer energon
+    * to that square, we throw an exception; hence we explicitly try for that
+    * condition.
+    **/
+    private void heal() {
+        
+        // To whom are we going to attempt to transfer energon?
+        RobotInfo transferTarget = null;
+       
+        RobotInfo closestGroundRobot = 
+            UtilityFunctions.min(kb.friendlyGroundRobots, 0, kb.friendlyGroundRobotsSize, distanceComp);
+        RobotInfo closestAirRobot = 
+            UtilityFunctions.min(kb.friendlyAirRobots, 0, kb.friendlyAirRobotsSize, distanceComp);
+        
+        transferTarget = closestGroundRobot;
+        // TODO: take into account air robot
+   
+        // No suitable targets to heal
+        if (transferTarget == null) {
+            if (DEBUG) {
+                System.out.println("No targets are suitable for healing");
+            }
+            return;
+        }
+        
+        MapLocation curLoc = rc.getLocation();
+        MapLocation healingTargetLoc = transferTarget.location;
+    
+        // We can transfer the energon
+        if (curLoc.equals(healingTargetLoc) || 
+            curLoc.isAdjacentTo(healingTargetLoc)) {
+            try {
+                double amountEnergonToTransfer = 
+                    calculateEnergonToTransfer(rc, transferTarget);
+                // At what height is the robot we're trying to heal?
+                RobotLevel levelOfTransferTarget = transferTarget.type.isAirborne() ? 
+                                                    RobotLevel.IN_AIR :
+                                                    RobotLevel.ON_GROUND;
+                // Finally, we know how much, where, and at what height to
+                // transfer energon.  Go ahead and try it
+                rc.transferEnergon(amountEnergonToTransfer, 
+                                    healingTargetLoc, 
+                                    levelOfTransferTarget);
+                rc.yield();
+            }
+            // The transfer failed for some reason
+            catch (GameActionException e) {
+                if (DEBUG) {
+                    System.out.println("Attempted to heal a robot " + 
+                                        transferTarget + " with out " +
+                                        "of date information; robot must " +  
+                                        "have moved");
+                    e.printStackTrace();
+                }
+            }
+        }
+        
+        // We are not adjacent; we need to move towards the robot
+        else {
+            
+            if (DEBUG) {
+                System.out.println("Trying to heal " + transferTarget + 
+                                    " but it's too far away.");
+            }
+            
+            // If we can move this turn, do so
+            
+            // Else ... 
+            
+            
+        }
+    
+        
+    }
+    
+    private double calculateEnergonToTransfer(RobotController rc, RobotInfo transferTarget) {
+        // Give them either how much energon they need to get back to full 
+        // health, or however much we can afford to give them without depleting
+        // our reserves too much
+        return Math.min(getDamage(transferTarget), 
+                        rc.getEventualEnergonLevel() - MIN_ENERGON_RESERVE);
+                                    
+    }
+    
+    
     
 }
